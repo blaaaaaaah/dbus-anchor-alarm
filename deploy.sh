ed71bcc7f977b4e5796c9c6e3f924c60763768b2
Thomas <thomas@dubois.xyz>
2025-06-10 09:54:07 -0400

    updated comments

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ echo "$git_revision" > VERSION
 
 rsync -vr --exclude=".*" * root@victron.matsu:/data/dbus-anchor-alarm
 
-# run by doing :
+# on first time, ssh into the cerbo and run /data/dbus-anchor-alarm/setup.sh to install symlinks
+
+# run as service by doing
 # cd /opt/victronenergy/service/
 # svc -u dbus-anchor-alarm
+
+# or run directly python3 anchor_alarm_service.py
